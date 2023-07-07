e72c4b22debfc4de6f55b0ce52cf52c809e94e17
Muhammad Afifudin <muhafifudin2306@gmail.com>
2023-07-07 14:18:22 +0700

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="section">
+        <div class="container mt-5">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-12 col-md-6 col-lg-5">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h4>{{ __('Login') }}</h4>
+                        </div>
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="email">{{ __('Email') }}</label>
+                                    <input id="email" type="email" placeholder="example@example.com"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="d-block">
+                                        <label for="password" class="control-label">{{ __('Password') }}</label>
+                                        <div class="float-right">
+                                            @if (Route::has('password.request'))
+                                                <a class="text-small" href="{{ route('password.request') }}">
+                                                    {{ __('Forgot Your Password?') }}
+                                                </a>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror pwstrength"
+                                        placeholder="******" data-indicator="pwindicator" name="password" required
+                                        autocomplete="current-password">
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="custom-control custom-checkbox">
+                                        <input class="custom-control-input" type="checkbox" name="remember" id="remember-me"
+                                            {{ old('remember') ? 'checked' : '' }}>
+                                        <label class="custom-control-label"
+                                            for="remember-me">{{ __('Remember Me') }}</label>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                        {{ __('Login') }}
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>SIB Pro</title>
+
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/bootstrap.min.css') }}">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+</head>
+
+<body>
+    <div id="app">
+        @yield('content')
+    </div>
+    <!-- General JS Scripts -->
+    <script src="{{ asset('assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+</body>
+
+</html>
