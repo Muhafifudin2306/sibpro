fe68f18af6c71bcd796bf86226f20544a68edcef
Muhammad Afifudin <muhafifudin2306@gmail.com>
2023-11-13 10:01:03 +0700

    Finishing Audit General Setting

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Year;
 use Illuminate\Http\Request;
 use App\Models\Notification;
 use App\Models\StudentClass;
+use Illuminate\Support\Facades\Auth;
 
 class StudentController extends Controller
 {
@@ -34,20 +36,41 @@ class StudentController extends Controller
     public function updateAllClass(Request $request, $id)
     {
         $users = User::where('class_id', $id)->get();
+
+        $class = StudentClass::find($id);
+
         foreach ($users as $user) {
             $user->class_id = $request->input('class_id');
             $user->save();
         }
 
+        $activeYearId = Year::where('year_status', 'active')->value('id');
+        $years = Year::find($activeYearId);
+
+        Notification::create([
+            'notification_content' => Auth::user()->name . " " . "mengedit semua siswa kelas" . " " . $class->class_name . " " . $years->year_name,
+            'notification_status' => 0
+        ]);
+
         return response()->json([
             'message' => 'Data updated successfully'
-        ], 201); // 201 updated
+        ], 201);
     }
 
     public function destroyAllStudent($id)
     {
+        $class = StudentClass::find($id);
+
         User::where('class_id', $id)
             ->delete();
+
+            $activeYearId = Year::where('year_status', 'active')->value('id');
+        $years = Year::find($activeYearId);
+
+        Notification::create([
+            'notification_content' => Auth::user()->name . " " . "menghapus semua siswa kelas" . " " . $class->class_name . " " . $years->year_name,
+            'notification_status' => 0
+        ]);
 
         return response()->json([
             'message' => 'Data deleted successfully'
