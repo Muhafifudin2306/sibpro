38c0804107de48f0b297562d3df54e45fe7f7fbb
Muhammad Afifudin <muhafifudin2306@gmail.com>
2023-11-18 07:26:17 +0700

    solved problem credit

--- app/Http/Controllers/CreditController.php
+++ app/Http/Controllers/CreditController.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Auth;
 
 class CreditController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     public function index()
     {
