8f9b1f7e8849415aed9b20a9c1555f7f581ebf26
Takamanu <fauzanalivijsma@gmail.com>
2024-05-31 01:28:17 +0700

    feat: bulk import siswa excel

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,6 +15,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
+    protected $table = "users";
+
     /**
      * The attributes that are mass assignable.
      *
